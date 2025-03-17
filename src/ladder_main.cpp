923e2a709c809e0dbe19c1c3086adb6254ca1e7a
Marmalicious <thomaln5@uci.edu>
2025-03-17 00:14:39 +0000

    removed copying

--- src/ladder_main.cpp
+++ src/ladder_main.cpp
@@ -1,8 +1,9 @@
 #include "ladder.h"
 
 int main() {
-    //set<string> list;
-    //load_words(list, "src/word.txt");
+    set<string> list;
+    load_words(list, "src/words.txt");
     //print_word_ladder(generate_word_ladder("dog", "cat", list));
-    verify_word_ladder();
+    //verify_word_ladder();
+    cout << generate_word_ladder("awake", "sleep", list).size();
 }
